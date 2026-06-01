c2d4fab82a819de870388fc06959ebadca91a6f9
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-06-01 20:36:07 +0100

    portfolio project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+        <nav>
+            <div>
+                <span>Chinedu</span>
+            </div>
+
+            <div>
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="/projects">Projects</a>
+                <a href="/skills">Skills</a>
+                <a href="/contacts">Contacts</a>
+            </div>
+            
+        </nav>
+  )
+}
+
+export default Navbar
