bebb6baeef5ea5134d0a43bf45b6e1c73cdcea34
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-06-02 06:22:01 +0100

    worked on the navbar.jsx, App.jsx, App.css, index.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,20 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-        <nav>
+        <nav className='navbar'>
             <div>
-                <span>Chinedu</span>
+                <span>Chinedu Daniel</span>
             </div>
 
-            <div>
-                <a href="/">Home</a>
+            <div className='nav-links'>
+                <a href="/" className='home'>Home</a>
                 <a href="/about">About</a>
                 <a href="/projects">Projects</a>
                 <a href="/skills">Skills</a>
                 <a href="/contacts">Contacts</a>
+            </div>
+            <div className='nav-button'>
+                <button>Let's Connect</button>
             </div>
             
         </nav>
