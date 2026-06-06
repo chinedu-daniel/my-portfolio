5b2c289103709aec424a9fcd4fbedf113ed60514
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-06-06 10:08:10 +0100

    added extra .map() and .slice() to project,jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,30 +1,36 @@
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 import pic from "../assets/images/ken.png";
+import livescore from "../assets/images/livescore.png";
+import finvault from "../assets/images/finvault.png";
 
 const projects = [
-    {
-        image: pic,
-        title: "Real Estate App",
-        description: "A modern real estate application for buying and selling properties.",
-        value: "Web Development",
-    },
-    {
-        image: pic,
-        title: "Real Estate App",
-        description: "A modern real estate application for buying and selling properties.",
-        value: "Web Development",
-    },
-    {
-        image: pic,
-        title: "Real Estate App",
-        description: "A modern real estate application for buying and selling properties.",
-        value: "Web Development",
-    },
-]
+  {
+    image: pic,
+    title: "Real Estate App",
+    description:
+      "A modern real estate application for buying and selling properties.",
+    value: "Web Development",
+  },
+  {
+    image: livescore,
+    title: "Live Score App",
+    description: "A modern live score application for sports live update.",
+    value: "Web Development",
+  },
+  {
+    image: finvault,
+    title: "FinTech App",
+    description:
+      "A modern fintech application for swift and reliable transaction.",
+    value: "Web Development",
+  },
+];
 
 const Projects = () => {
+  const [showMore, setShowMore] = useState(false);
+
   return (
     <section className="project-section" id="projects">
       <div className="project-text-container">
@@ -34,8 +40,9 @@ const Projects = () => {
         </div>
 
         <div className="project-span">
-          <span>
-            View All Projects <FontAwesomeIcon icon={faArrowRight} />
+          <span onClick={() => setShowMore(!showMore)}>
+            {showMore ? "Hide Projects" : "View All Projects"}{" "}
+            <FontAwesomeIcon icon={faArrowRight} />
           </span>
         </div>
       </div>
@@ -48,13 +55,31 @@ const Projects = () => {
             </div>
 
             <div className="project-info">
-                <h3 className="project-title">{item.title}</h3>
-                <p className="project-description">{item.description}</p>
-                <p className="project-value">{item.value}</p>
+              <h3 className="project-title">{item.title}</h3>
+              <p className="project-description">{item.description}</p>
+              <p className="project-value">{item.value}</p>
             </div>
           </div>
         ))}
       </div>
+
+      {showMore && (
+        <div className="project-grid">
+        {projects.slice(0).map((item, index) => (
+          <div key={index} className="project-card">
+            <div className="project-image">
+              <img src={item.image} alt="project image" />
+            </div>
+
+            <div className="project-info">
+              <h3 className="project-title">{item.title}</h3>
+              <p className="project-description">{item.description}</p>
+              <p className="project-value">{item.value}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      )}
     </section>
   );
 };
